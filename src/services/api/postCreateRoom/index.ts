dbf3f6b74cf4464f8dc3c547c26be7b47c24ceee
Laura Luchiari <lauraluchiari@hotmail.com>
2025-10-08 21:45:51 -0300

    refactor: remove diretório api

--- src/services/api/postCreateRoom/index.ts
+++
@@ -1,6 +0,0 @@
-import { api } from "..";
-import type { PostCreateRoomPayload } from "./types";
-
-export async function postCreateRoom(payload: PostCreateRoomPayload) {
-  await api.post("/rooms", payload);
-}
--- src/services/api/postEnterRoom/index.ts
+++
@@ -1,9 +0,0 @@
-import { api } from "..";
-import type { PostEnterRoomPayload } from "./types";
-
-export async function postEnterRoom(
-  code: string,
-  payload: PostEnterRoomPayload
-) {
-  await api.post(`/rooms/${code}/users`, payload);
-}
